262153145c77dd9c153c49f5a8dfeed6f7fd885e
1596aishu <55698607+1596aishu@users.noreply.github.com>
2020-06-07 13:37:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+// export default App;
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+class App extends Component {
+    render() {
+    const myfirstelement = document.getElementById('root');
+
+    
+    return(myfirstelement);
+    }
+}
+export default App;
