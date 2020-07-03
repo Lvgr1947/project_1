5e635bfe9b0d857d7b00108df8b146e245038a5f
1596aishu <55698607+1596aishu@users.noreply.github.com>
2020-07-03 13:14:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-
-// export default App;
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
-class App extends Component {
-    render() {
-    const myfirstelement = document.getElementById('root');
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './Components/Home';
+import Course from './Components/course';
+import Nodue from './Components/nodue';
+import Navbar from './Components/customNavbar';
+import transcripts from './Components/transcripts';
 
-    
-    return(myfirstelement);
-    }
+
+class App extends Component{
+  render()
+{  return (
+    <Router>
+      <Navbar />
+      <div>      
+        <Route exact path = "/" component={Home}/>
+        <Route path = "/course" component={Course}/>
+        <Route path = "/nodue" component={Nodue}/>        
+        <Route path = "/transcripts" component={transcripts}/>
+        
+      </div>
+    </Router>
+  );
 }
+}
+
 export default App;
